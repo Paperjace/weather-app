68cbcad1f4ab6ebccbec656b4e25c3def33be00e
Jason Hurley <jhurley@infoinc.com>
2020-01-16 19:56:52 -0500

    checking in progress

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/8ec26beb8b911b5722cc52622831929b/' + latitude + ',' + longitude
 
-    request({url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service.')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location.')
         } else {
-            const currentWeather = response.body
+            const currentWeather = body
             const data = currentWeather.daily.data[0].summary + " It is currently " + currentWeather.currently.temperature + " degrees out. There is a " + currentWeather.currently.precipProbability + "% chance of rain."
             
             callback(undefined, data)
